7d1c4b0b08c199dfa7477a702db5fbf3bb6aacc6
xiong <1095939843@qq.com>
2017-07-24 15:44:58 +0800

    爬去淘宝美食信息

---
+++ config.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+"""
+__mktime__ = '2017/7/21'
+__author__ = 'bonree'
+__filename__ = 'config'
+"""
+MONGO_URL = 'localhost'
+MONGO_DB = 'taobao'
+MONGO_TABLE = 'taobao'
+
+SERVICE_ARGS = ['--load-images=false', '--disk-cache=true']
+
+KEYWORD = '美食'
